760d2dff564a53ce4503b4406ef368495fffef79
Kumar Abhinav <abhinav9011032@gmail.com>
2024-09-03 22:15:41 -0700

    Update customer.ts

--- models/customer.ts
+++ models/customer.ts
@@ -1,5 +1,6 @@
 import { type IBaseCollectionFields } from './base'
 
+// model to represent Customer table in mongo
 export interface Customer extends IBaseCollectionFields {
   name: string
   email: string
