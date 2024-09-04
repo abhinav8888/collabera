e6fe41a9b0994f5adf7d78a67c0c95da0597cb2d
Kumar Abhinav <abhinav9011032@gmail.com>
2024-09-03 22:21:46 -0700

    Update base.ts

--- models/base.ts
+++ models/base.ts
@@ -1,5 +1,6 @@
 import { type ObjectId } from 'mongodb'
 
+// base class to represent all collection documents in mongodb
 export interface IBaseCollectionFields {
   _id: ObjectId
   createdAt?: Date
